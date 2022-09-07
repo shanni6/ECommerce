2552dcd471ded1ca3c601123dbba8731b89ef2c5
shanni6 <240686886@qq.com>
2022-09-07 12:40:36 +0800

    layout finish

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+ 
+const Layout = ({children}) => {
+    return <div className="layout">
+        <Head>
+            <title>JS Mastery Store</title>
+        </Head>
 
-const Layout = () => {
-  return <div>Layout</div>;
+        <header>
+            <Navbar/>
+        </header>
+
+        <main className="main-container">
+            {children}
+        </main>
+
+        <footer>
+            <Footer/>
+        </footer>
+    </div>;
 };
 
 export default Layout;
+
