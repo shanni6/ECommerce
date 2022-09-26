7b52bd6178b4aaffec7eb8f345a3dd55837b6d7a
shanni6 <240686886@qq.com>
2022-09-26 22:05:44 +0800

    Context Logic finished

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    AiOutlineMinus,
+    AiOutlinePlus,
+    AiFillStar,
+    AiOutlineStar,
+} from "react-icons/ai";
 import { client, urlFor } from "../../lib/client";
-
+import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
     return (
         <div>
@@ -11,18 +20,72 @@ const ProductDetails = ({ product, products }) => {
                     <div className="image-container">
                         <img
                             className="product-detail-image"
-                            src={urlFor(image && image[0])}
+                            src={urlFor(image && image[index])}
                         />
                     </div>
 
-                    {/* <div className="small-image-container">
-                    {image?.map((item,i)=>(
-                        <img src={urlFor(item)} className="" onMouseEnter=""/>
-                    ))}
-                        </div> */}
+                    <div className="small-image-container">
+                        {image?.map((item, i) => (
+                            <img
+                                src={urlFor(item)}
+                                onMouseEnter={() => setIndex(i)}
+                                className={
+                                    i === index
+                                        ? "small-image selected-image"
+                                        : "small-image"
+                                }
+                            />
+                        ))}
+                    </div>
                 </div>
-                <div className="products-details=desc">
+                <div className="product-detail-desc">
                     <h1>{name}</h1>
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
+                        </div>
+                        <p>(20)</p>
+                    </div>
+                    <h4> Details</h4>
+                    <p>{details}</p>
+                    <p className="price">${price} </p>
+                    <div className="quantity">
+                        <h3>Quanity:</h3>
+                        <p className="quantity-desc">
+                            <span className="minus" onClick={decQty}>
+                                <AiOutlineMinus />{" "}
+                            </span>
+                            <span className="num"> {qty}</span>
+                            <span className="plus" onClick={incQty}>
+                                {" "}
+                                <AiOutlinePlus />{" "}
+                            </span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button
+                            type="button"
+                            className="add-to-cart"
+                            onClick={() => onAdd(product, qty)}
+                        >
+                            Add to Cart
+                        </button>
+                        <button className="buy-now">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id} product={item} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,10 +95,8 @@ const ProductDetails = ({ product, products }) => {
 export const getStaticPaths = async () => {
     const query = `*[_type == "product"] {
     slug {
-      current
-    }
-  }
-  `;
+current
+    }}`;
 
     const products = await client.fetch(query);
 
